47dc83b499a1a57b2da38b0c6ac850d461b64b51
OmAximani0 <aximaniom@gmail.com>
2022-05-29 18:44:19 +0530

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'quote.dart';
+import 'quote_card.dart';
+
+void main() => runApp(MaterialApp(
+      home: QuoteList(),
+    ));
+
+// ignore: use_key_in_widget_constructors
+class QuoteList extends StatefulWidget {
+  @override
+  _QuoteListState createState() => _QuoteListState();
+}
+
+class _QuoteListState extends State<QuoteList> {
+  List<Quote> quotes = [
+    Quote('Be yourself; everyone else is already taken', 'Oscar Wilde'),
+    Quote('I have nothing to declare except my genius', 'Oscar Wilde'),
+    Quote('The truth is rarely pure and never simple', 'Oscar Wilde'),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: const Text('Awesome Quotes'),
+        centerTitle: true,
+        backgroundColor: Colors.redAccent,
+      ),
+      body: Column(
+        children: quotes.map((quote) => QuoteCard(
+          quote: quote,
+          delete: () {
+            setState(() {
+              quotes.remove(quote);
+            });
+          }
+        )).toList(),
+      ),
+    );
+  }
+}
